d41ec7b91538f331d9a9ebdf27f4836839108c1d
Яков <yakovzubets@gmai.com>
2021-08-10 09:41:29 +0300

    new apps and models genres, titles, categories

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genres(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Titles(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.IntegerField()
+    description = models.TextField(max_length=400)
+    genre = models.ForeignKey(
+        Genres, on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='genre'
+    )
+    category = models.ForeignKey(
+        Categories, on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='category'
+    )
+
+    def __str__(self):
+        return self.name
