dc8a928e16d8d58080a9fcd3d9e7d2fc37742d3d
Alexander Polyakov <devops.edc@gmail.com>
2021-08-13 16:09:39 +0300

    Added basic structure of models: Comment and Review

---
+++ api_yamdb/api_yamdb/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Comment(models.Model):
+    review_id = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+
+class Review(models.Model):
+    title_id = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='reviews')
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='reviews')
+    score = models.FloatField()
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
