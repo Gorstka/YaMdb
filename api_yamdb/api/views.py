690555c5658bcd4167cea73bf02011ae00b51a0b
Alexander Polyakov <devops.edc@gmail.com>
2021-08-14 11:47:56 +0300

    Added viewsets, serializers

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets
+from .serializers import CommentSerializer, ReviewSerializer
+from reviews.models import Comment, Review
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,15 +1,5 @@
 from django.db import models
 
-
-class Comment(models.Model):
-    review = models.ForeignKey(
-        Review, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-
-
 class Review(models.Model):
     title = models.ForeignKey(
         Title, on_delete=models.CASCADE, related_name='reviews')
@@ -19,3 +9,11 @@ class Review(models.Model):
     score = models.FloatField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
 
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
