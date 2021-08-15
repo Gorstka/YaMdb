f3d5d9fe2cbce131ae68847fe69511e35ba19f59
Яков <yakovzubets@gmai.com>
2021-08-15 18:25:12 +0300

    permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -4,6 +4,6 @@ from rest_framework import permissions
 class IsAdminOrReadOnly(permissions.BasePermission):
     message = 'Изменение контента разрешено только администратору!'
 
-    def has_object_permission(self, request, view, obj):
-        return request.method in permissions.SAFE_METHODS or (
-            request.user == request.user.is_admin)
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_staff)
